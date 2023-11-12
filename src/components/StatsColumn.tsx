6ec041644531b9be5d862a8aaaf114ca49fddb7e
anushkadeshpande <anushkaa5000@gmail.com>
2023-11-12 07:47:14 +0530

    feat: Added UI components

---
+++ src/components/StatsColumn.tsx
@@ -0,0 +1,69 @@
+// Import External Dependencies
+import React from 'react'
+import {View, Text} from "@nodegui/react-nodegui"
+
+// Set Types
+type StatsColumnProps = {
+    label: string,
+    usage: number,
+    color: string
+}
+
+const StatsColumn = (props : any) => {
+    // Destructure props
+    const {usage, color, label} = props
+
+    // Create Label with usage amount and percentage
+    const percentageTextLabel = `${label} ${Math.round(usage * 100) / 100}%`
+
+    // Create Dynamic Style Sheet
+    const dynamicStyle = `
+        height: ${usage};
+        background-color: ${color};
+    `
+
+    return (
+        <View id="statsContainer" styleSheet={statsContainer}>
+            <View id="columnContainer" styleSheet={columnContainer}>   
+                <View id="innerColumn" styleSheet={dynamicStyle}></View>
+            </View>
+            <Text id="statsLabel" styleSheet={statsLabel}>{percentageTextLabel}</Text>
+        </View>
+    )
+}
+
+const statsContainer = `
+    #statsContainer {
+        height: '140';
+        text-align:center;
+        justify-content: 'center';
+        align-items: 'center';
+        justify-content: 'space-between';
+        width: 100%;
+        flex: 1 0 100%;
+        margin-horizontal: 5px;
+    }
+`
+
+const columnContainer = `
+    #columnContainer{
+        height: 100%;
+        flex: 1 0 100%;
+        flex-direction: column-reverse;
+        background-color: #747474;
+        width: 100%;
+    }
+`
+
+const statsLabel = `
+    #statsLabel {
+        height: 40;
+        color: white;
+        font-size: 14px;
+        width: 100%;        
+        qproperty-alignment: 'AlignCenter';
+        color: white;
+    }
+`
+
+export default StatsColumn
